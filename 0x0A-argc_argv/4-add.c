f3df76fe4c5806be78ae36febef22b9b5d640950
Robert-majstro <roberteleh@gmail.com>
2023-03-10 18:19:31 -0800

    4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,58 +1,84 @@
-#include <string.h>
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#define UNUSED(x) (void)(x)
+	#include <stdlib.h>
+	#include <ctype.h>
+	#include <string.h>
 /**
- * StringCheck - checks string
- * @s: string to check
- * Return: boolean
- */
-int StringCheck(char *s)
-{
-int i = 0;
+	 * check_num - check - string there are digit
+	 * @str: array str
+	 *
+	 * Return: Always 0 (Success)
+	 */
+	int check_num(char *str)
+	{
+		/*Declaring variables*/
+		unsigned int count;
 
-for (; s[i] != '\0'; i++)
-{
-if (!isdigit(s[i]))
-{
-return (0);
-}
-}
-return (1);
-}
+
+		count = 0;
+		while (count < strlen(str)) /*count string*/
+
+
+		{
+			if (!isdigit(str[count])) /*check if str there are digit*/
+			{
+				return (0);
+			}
+
+
+			count++;
+		}
+		return (1);
+	}
+
+
 /**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- *Return: always 0
- */
-int main(int argc, char  *argv[])
-{
-int i;
-int result = 0;
+	 * main - Print the name of the program
+	 * @argc: Count arguments
+	 * @argv: Arguments
+	 *
+	 * Return: Always 0 (Success)
+	 */
 
-if (argc > 1)
-{
-for (i = 1; i < argc; i++)
-{
-if (StringCheck(argv[i]))
-{
-result += atoi(argv[i]);
-}
-else
-{
-printf("Error\n");
-return (1);
-}
-}
-printf("%d\n", result);
+
+	int main(int argc, char *argv[])
+
+
+	{
+
+
+		/*Declaring variables*/
+		int count;
+		int str_to_int;
+		int sum = 0;
+
+
+		count = 1;
+		while (count < argc) /*Goes through the whole array*/
+		{
+			if (check_num(argv[count]))
+
+
+			{
+				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+				sum += str_to_int;
+			}
+
+
+			/*Condition if one of the number contains symbols that are not digits*/
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+
+			count++;
+		}
+
+
+		printf("%d\n", sum); /*print sum*/
+
+
 return (0);
-}
-else
-{
-printf("%d\n", 0);
-return (1);
-}
-
 }
